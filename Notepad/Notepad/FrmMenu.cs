e121a2f5eb31277d4446eace87954a5bb66e010f
Behram CENGİZOĞLU <64408522+BehramCENGIZOGLU@users.noreply.github.com>
2021-05-01 19:04:18 +0300

    Notepad

---
+++ Notepad/Notepad/FrmMenu.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+            saveFileDialog1.Filter = "Only text|*.rtf";
+            saveFileDialog1.FileName = "isimsiz.txt";
+            saveFileDialog1.Title = "Kaydet";
+            saveFileDialog1.DefaultExt = "rtf";
+            saveFileDialog1.InitialDirectory = "C:";
+        }
+
+        private void miYeni_Click(object sender, EventArgs e)
+        {
+            if (rtbAnaMetin.Text != "")
+            {
+                var result = MessageBox.Show("Kapatmadan Önce Kaydetmek İstermisiniz?","Onay",MessageBoxButtons.YesNoCancel,MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    var saveres = saveFileDialog1.ShowDialog();
+                }
+
+            }
+
+            rtbAnaMetin.Text = "";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            SetStyle(rtbAnaMetin.SelectionFont.Size);
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            SetStyle(rtbAnaMetin.SelectionFont.Size);
+           
+        }
+
+        private void SetStyle(float fontSize)
+        {
+            rtbAnaMetin.SelectionFont = new Font(rtbAnaMetin.Font, rtbAnaMetin.Font.Style);
+
+            if (checkBox2.Checked)
+            {
+                rtbAnaMetin.SelectionFont = new Font(rtbAnaMetin.Font.Name, fontSize, rtbAnaMetin.SelectionFont.Style | FontStyle.Italic);
+            }
+            else
+            {
+                rtbAnaMetin.SelectionFont = new Font(rtbAnaMetin.Font.Name, fontSize, rtbAnaMetin.SelectionFont.Style | FontStyle.Regular);
+
+            }
+
+            if (checkBox1.Checked)
+            {
+                rtbAnaMetin.SelectionFont = new Font(rtbAnaMetin.Font.Name, fontSize, rtbAnaMetin.SelectionFont.Style | FontStyle.Bold);
+            }
+            else
+            {
+                rtbAnaMetin.SelectionFont = new Font(rtbAnaMetin.Font.Name, fontSize, rtbAnaMetin.SelectionFont.Style | FontStyle.Regular);
+
+            }
+
+            rtbAnaMetin.Focus();
+        }
+
+        private void miKaydet_Click(object sender, EventArgs e)
+        {
+          
+             
+                    var saveres = saveFileDialog1.ShowDialog();
+             
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter Kayit = new StreamWriter(saveFileDialog1.FileName);
+                Kayit.WriteLine(rtbAnaMetin.Rtf);
+                Kayit.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var res = colorDialog1.ShowDialog();
+
+            if(res == DialogResult.OK)
+            {
+                rtbAnaMetin.SelectionColor = colorDialog1.Color;
+            }
+        }
+
+        private void cbFontSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SetStyle(float.Parse(cbFontSize.SelectedItem.ToString()));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int index = 0;
+            string metin = rtbAnaMetin.Text;
+            rtbAnaMetin.Text = "";
+            rtbAnaMetin.Text = metin;
+            while (rtbAnaMetin.Text.IndexOf(tbBul.Text, index)!= -1)
+{
+                int konum = rtbAnaMetin.Text.IndexOf(tbBul.Text, index);
+                rtbAnaMetin.Select( konum, tbBul.Text.Length);
+                rtbAnaMetin.SelectionBackColor = Color.Yellow;
+                index = konum + 1;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            rtbAnaMetin.Text =  rtbAnaMetin.Text.Replace(tbBul.Text, tbDegistir.Text);
+        }
+
+       
+    }
+}
---
+++ Notepad/Notepad/FrmMenu.designer.cs
@@ -0,0 +1,242 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.miYeni = new System.Windows.Forms.ToolStripMenuItem();
+            this.miYukle = new System.Windows.Forms.ToolStripMenuItem();
+            this.miKaydet = new System.Windows.Forms.ToolStripMenuItem();
+            this.miCikis = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.rtbAnaMetin = new System.Windows.Forms.RichTextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbFontSize = new System.Windows.Forms.ComboBox();
+            this.tbBul = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tbDegistir = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(875, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miYeni,
+            this.miYukle,
+            this.miKaydet,
+            this.miCikis});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // miYeni
+            // 
+            this.miYeni.Name = "miYeni";
+            this.miYeni.Size = new System.Drawing.Size(138, 26);
+            this.miYeni.Text = "Yeni";
+            this.miYeni.Click += new System.EventHandler(this.miYeni_Click);
+            // 
+            // miYukle
+            // 
+            this.miYukle.Name = "miYukle";
+            this.miYukle.Size = new System.Drawing.Size(138, 26);
+            this.miYukle.Text = "Yükle";
+            // 
+            // miKaydet
+            // 
+            this.miKaydet.Name = "miKaydet";
+            this.miKaydet.Size = new System.Drawing.Size(138, 26);
+            this.miKaydet.Text = "Kaydet";
+            this.miKaydet.Click += new System.EventHandler(this.miKaydet_Click);
+            // 
+            // miCikis
+            // 
+            this.miCikis.Name = "miCikis";
+            this.miCikis.Size = new System.Drawing.Size(138, 26);
+            this.miCikis.Text = "Çıkış";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 43);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(61, 21);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Kalın";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // rtbAnaMetin
+            // 
+            this.rtbAnaMetin.Location = new System.Drawing.Point(12, 70);
+            this.rtbAnaMetin.Name = "rtbAnaMetin";
+            this.rtbAnaMetin.Size = new System.Drawing.Size(851, 394);
+            this.rtbAnaMetin.TabIndex = 3;
+            this.rtbAnaMetin.Text = "";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(79, 43);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(58, 21);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "İtalik";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(784, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Renk";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbFontSize
+            // 
+            this.cbFontSize.FormattingEnabled = true;
+            this.cbFontSize.Items.AddRange(new object[] {
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "14",
+            "16",
+            "18",
+            "20",
+            "24",
+            "28",
+            "32",
+            "48",
+            "64"});
+            this.cbFontSize.Location = new System.Drawing.Point(657, 38);
+            this.cbFontSize.Name = "cbFontSize";
+            this.cbFontSize.Size = new System.Drawing.Size(121, 24);
+            this.cbFontSize.TabIndex = 6;
+            this.cbFontSize.SelectedIndexChanged += new System.EventHandler(this.cbFontSize_SelectedIndexChanged);
+            // 
+            // tbBul
+            // 
+            this.tbBul.Location = new System.Drawing.Point(143, 42);
+            this.tbBul.Name = "tbBul";
+            this.tbBul.Size = new System.Drawing.Size(100, 22);
+            this.tbBul.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(249, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Bul";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(436, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Değiştir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbDegistir
+            // 
+            this.tbDegistir.Location = new System.Drawing.Point(330, 40);
+            this.tbDegistir.Name = "tbDegistir";
+            this.tbDegistir.Size = new System.Drawing.Size(100, 22);
+            this.tbDegistir.TabIndex = 7;
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 476);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tbDegistir);
+            this.Controls.Add(this.tbBul);
+            this.Controls.Add(this.cbFontSize);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.rtbAnaMetin);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMenu";
+            this.Text = "Ana Ekran";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem miYeni;
+        private System.Windows.Forms.ToolStripMenuItem miYukle;
+        private System.Windows.Forms.ToolStripMenuItem miKaydet;
+        private System.Windows.Forms.ToolStripMenuItem miCikis;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox rtbAnaMetin;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbFontSize;
+        private System.Windows.Forms.TextBox tbBul;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox tbDegistir;
+    }
+}
